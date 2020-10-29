dc9843cf4b8165cb4042a46ba3856c32d9dda573
Halime123 <halimecobanli123@gmail.com>
2020-10-29 15:27:32 -0700

    abc

--- src/com/syntax/class15/Sample.java
+++
@@ -1,9 +0,0 @@
-package com.syntax.class15;
-
-public class Sample {
-    public static void main(String[] args) {
-        String firstName = "Ebrar";
-        String lastName = " Gur";
-        System.out.println(firstName + lastName);
-    }
-}
