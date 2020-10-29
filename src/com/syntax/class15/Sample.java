8c242105f1396517f9924be70743709995c6b9eb
Halime123 <halimecobanli123@gmail.com>
2020-10-29 15:24:07 -0700

    class15 updated

---
+++ src/com/syntax/class15/Sample.java
@@ -0,0 +1,9 @@
+package com.syntax.class15;
+
+public class Sample {
+    public static void main(String[] args) {
+        String firstName = "Ebrar";
+        String lastName = " Gur";
+        System.out.println(firstName + lastName);
+    }
+}
